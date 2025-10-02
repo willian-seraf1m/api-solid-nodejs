1e480d72cad6399e934cdba34aec7564ed3c5746
willian <willianserafim2017@gmail.com>
2025-10-02 15:44:52 -0300

    feat: implement factories for use cases to reduce boilerplate

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,9 +1,11 @@
 import type { FastifyInstance } from 'fastify'
 import { register } from './controllers/register'
 import { authenticate } from './controllers/authenticate'
+import { profile } from './controllers/profile'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/sessions', authenticate)
-
   app.post('/users', register)
+
+  app.get('/me', profile)
 }
--- src/use-cases/factories/make-register-use-case.ts
+++ src/use-cases/factories/make-register-use-case.ts
@@ -3,7 +3,7 @@ import { RegisterUserCase } from '../register'
 
 export function makeRegisterUseCase() {
   const usersRepository = new PrismaUsersRepository()
-  const registerUseCase = new RegisterUserCase(usersRepository)
+  const useCase = new RegisterUserCase(usersRepository)
 
-  return registerUseCase
+  return useCase
 }
